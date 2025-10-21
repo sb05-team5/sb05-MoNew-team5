0189c25577d1783ffbd5fb1eb6d5b29bd73ab16f
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-21 16:56:09 +0900

    first commit

---
+++ src/test/java/com/sprint/project/monew/sb05monewteam05/Sb05MoNewTeam05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.project.monew.sb05monewteam05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb05MoNewTeam05ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
