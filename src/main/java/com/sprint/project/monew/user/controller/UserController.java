3a0dc1b38ee3e4f0086ba21286289b93d1a43b65
Jelo777 <junheef1@gmail.com>
2025-10-22 18:44:07 +0900

    feat:User/update 기본기능구현

--- src/main/java/com/sprint/project/monew/user/controller/UserController.java
+++ src/main/java/com/sprint/project/monew/user/controller/UserController.java
@@ -3,11 +3,15 @@ package com.sprint.project.monew.user.controller;
 import com.sprint.project.monew.user.dto.UserDto;
 import com.sprint.project.monew.user.dto.UserLoginRequest;
 import com.sprint.project.monew.user.dto.UserRegisterRequest;
+import com.sprint.project.monew.user.dto.UserUpdateRequest;
 import com.sprint.project.monew.user.service.AuthService;
 import com.sprint.project.monew.user.service.UserService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,13 @@ public class UserController {
   @PostMapping("/login")
   public ResponseEntity<UserDto> login(@RequestBody UserLoginRequest userLoginRequest) {
     UserDto userDto = authService.login(userLoginRequest);
+    return ResponseEntity.status(HttpStatus.OK).body(userDto);
+  }
+
+  @PatchMapping(path = "{userId}")
+  public ResponseEntity<UserDto> update(@PathVariable("userId") UUID userId,
+                                        @RequestBody UserUpdateRequest request) {
+    UserDto userDto = userService.update(userId, request);
     return ResponseEntity.status(HttpStatus.OK).body(userDto);
   }
 }
--- src/main/java/com/sprint/project/monew/user/repository/UserRepository.java
+++ src/main/java/com/sprint/project/monew/user/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByNickname(String nickname);
 
   Optional<User> findByEmail(String email);
+
+  Optional<User> findById(UUID id);
 }
