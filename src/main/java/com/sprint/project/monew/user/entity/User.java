c19bf04f0f4b7e638d408e11b0152b8bbf63b755
Jelo777 <junheef1@gmail.com>
2025-10-22 13:34:52 +0900

    feat:User엔티티 생성

---
+++ src/main/java/com/sprint/project/monew/user/entity/User.java
@@ -0,0 +1,31 @@
+package com.sprint.project.monew.user.entity;
+
+import com.sprint.project.monew.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+  @Column(nullable = false, unique = true, length = 100)
+  private String email;
+
+  @Column(nullable = false, unique = true, length = 20)
+  private String nickname;
+
+  @Column(nullable = false, length = 100)
+  private String password;
+
+  @Column
+  private Instant deletedAt;
+}
