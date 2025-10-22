fcac1187f296583aba691d46a350393e81b6511e
Jelo777 <junheef1@gmail.com>
2025-10-22 18:04:32 +0900

    feat:User/login 기본기능구현

--- src/main/java/com/sprint/project/monew/user/controller/UserController.java
+++ src/main/java/com/sprint/project/monew/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sprint.project.monew.user.controller;
 
 import com.sprint.project.monew.user.dto.UserDto;
+import com.sprint.project.monew.user.dto.UserLoginRequest;
 import com.sprint.project.monew.user.dto.UserRegisterRequest;
+import com.sprint.project.monew.user.service.AuthService;
 import com.sprint.project.monew.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
   private final UserService userService;
+  private final AuthService authService;
 
   @PostMapping
   public ResponseEntity<UserDto> create(@RequestBody UserRegisterRequest userRegisterRequest) {
-    System.out.println("요청 body: " + userRegisterRequest);
     UserDto userDto = userService.create(userRegisterRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<UserDto> login(@RequestBody UserLoginRequest userLoginRequest) {
+    UserDto userDto = authService.login(userLoginRequest);
+    return ResponseEntity.status(HttpStatus.OK).body(userDto);
   }
 }
--- src/main/java/com/sprint/project/monew/user/repository/UserRepository.java
+++ src/main/java/com/sprint/project/monew/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sprint.project.monew.user.repository;
 
 import com.sprint.project.monew.user.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
 
   boolean existsByNickname(String nickname);
+
+  Optional<User> findByEmail(String email);
 }
