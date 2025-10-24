8bd1ec94bd9d2d64186f87a62679d4b9d30758dd
Jelo777 <junheef1@gmail.com>
2025-10-24 10:27:34 +0900

    feat:User Validation 유효성 검사 추가

--- src/main/java/com/sprint/project/monew/user/dto/UserUpdateRequest.java
+++ src/main/java/com/sprint/project/monew/user/dto/UserUpdateRequest.java
@@ -1,6 +1,11 @@
 package com.sprint.project.monew.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserUpdateRequest(
+    @NotBlank(message = "닉네임은 필수입니다")
+    @Size(min = 1, max = 20, message = "닉네임은 1자 이상 20자 이하여야 합니다")
     String newNickname
 ) {
 
