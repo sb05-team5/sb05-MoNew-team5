109d8bfe5ed8f31f6d3910a0017bac6a9f63a70d
loi2016 <loi2012@naver.com>
2025-10-30 13:32:44 +0900

    CommentRepository 커밋 오류(?) 해결

--- src/main/java/com/sprint/project/monew/comment/repository/CommentRepository.java
+++ src/main/java/com/sprint/project/monew/comment/repository/CommentRepository.java
@@ -15,15 +15,15 @@ import java.util.UUID;
 public interface CommentRepository
         extends JpaRepository<Comment, UUID>, CommentQueryRepository {
 
-//    @Lock(LockModeType.PESSIMISTIC_WRITE)
-//    @Query("select c from Comment c where c.id = : id")
-//    Optional<Comment> findForUpdate(@Param("id") UUID id);
-//
-//    List<Comment> findOrderByDate(
-//            UUID articleId, boolean sort, Instant dateCursor, UUID idCursor, int limit
-//    );
-//
-//    List<Comment> findOrderByLikes(
-//            UUID articleId, boolean sort, Integer likeCursor, UUID idCursor, int limit
-//    );
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Comment c where c.id = : id")
+    Optional<Comment> findForUpdate(@Param("id") UUID id);
+
+    List<Comment> findOrderByDate(
+            UUID articleId, boolean sort, Instant dateCursor, UUID idCursor, int limit
+    );
+
+    List<Comment> findOrderByLikes(
+            UUID articleId, boolean sort, Integer likeCursor, UUID idCursor, int limit
+    );
 }
