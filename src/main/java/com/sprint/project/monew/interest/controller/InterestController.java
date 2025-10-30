812f54440a69d3706130d66098eb6d338f575423
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-30 11:21:39 +0900

    feat: 관심사 페이지네이션 로직 변경

--- src/main/java/com/sprint/project/monew/interest/controller/InterestController.java
+++ src/main/java/com/sprint/project/monew/interest/controller/InterestController.java
@@ -2,26 +2,26 @@ package com.sprint.project.monew.interest.controller;
 
 import com.sprint.project.monew.common.CursorPageResponse;
 import com.sprint.project.monew.interest.dto.InterestDto;
-import com.sprint.project.monew.interest.dto.InterestQuery;
 import com.sprint.project.monew.interest.dto.InterestRegisterRequest;
 import com.sprint.project.monew.interest.dto.InterestUpdateRequest;
 import com.sprint.project.monew.interest.dto.SubscriptionDto;
 import com.sprint.project.monew.interest.service.InterestService;
 import com.sprint.project.monew.interest.service.SubscriptionService;
 import jakarta.validation.Valid;
+import java.time.Instant;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -45,9 +45,17 @@ public class InterestController {
   }
 
   @GetMapping
-  public ResponseEntity<CursorPageResponse<InterestDto>> findAll(@ModelAttribute InterestQuery query) {
-    CursorPageResponse<InterestDto> interests = interestService.findAll(query);
-    return ResponseEntity.status(HttpStatus.OK).body(interests);
+  public ResponseEntity<CursorPageResponse<InterestDto>> findAll(
+      @RequestParam(required = false) String keyword,
+      @RequestParam(defaultValue = "name") String orderBy,
+      @RequestParam(defaultValue = "ASC") String direction,
+      @RequestParam(required = false) String cursor,
+      @RequestParam(required = false) Instant after,
+      @RequestParam(defaultValue = "50") int size,
+      @RequestHeader("MoNew-Request-User-ID") UUID userId) {
+    CursorPageResponse<InterestDto> all = interestService.findAll(keyword, orderBy, direction,
+        cursor, after, size, userId);
+    return ResponseEntity.status(HttpStatus.OK).body(all);
   }
 
   @PatchMapping("{interestId}")
