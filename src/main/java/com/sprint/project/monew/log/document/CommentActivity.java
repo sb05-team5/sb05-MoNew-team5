45f3101d1e9eceff734ca5bf617fa80d3f256626
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-30 18:02:20 +0900

    feat: 댓글 활동 도큐먼트 추가

---
+++ src/main/java/com/sprint/project/monew/log/document/CommentActivity.java
@@ -0,0 +1,42 @@
+package com.sprint.project.monew.log.document;
+
+import jakarta.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class CommentActivity {
+
+  @Id
+  private String id = UUID.randomUUID().toString();
+
+  @Field("created_at")
+  private Instant createdAt;
+
+  @Field("article_id")
+  private String articleId;
+  private String articleTitle;
+
+  @Field("user_id")
+  private String userId;
+  private String userName;
+
+  private String content;
+  private int likeCount;
+
+
+
+
+
+}
