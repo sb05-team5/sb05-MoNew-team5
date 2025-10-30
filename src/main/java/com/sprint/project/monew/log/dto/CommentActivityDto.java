6683073f6b27a33104a43e73f9a9240ac20dc191
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-30 18:11:33 +0900

    feat: 댓글 활동 dto 추가

--- src/main/java/com/sprint/project/monew/log/dto/CommentActivityDto.java
+++ src/main/java/com/sprint/project/monew/log/dto/CommentActivityDto.java
@@ -1,8 +1,16 @@
 package com.sprint.project.monew.log.dto;
 
+import java.time.Instant;
 import lombok.Builder;
 
 @Builder(toBuilder = true)
 public record CommentActivityDto(
-
+    String id,
+    Instant createdAt,
+    String articleId,
+    String articleTitle,
+    String userId,
+    String userName,
+    String content,
+    int likeCount
 ) { }
