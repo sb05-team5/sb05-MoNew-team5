f7495679e54cbcb3188f9ab19e6b38fc8fe57f29
loi2016 <loi2012@naver.com>
2025-10-30 13:25:23 +0900

    Feature/comment : Controller, Comment, QueryRepo 수정/Repo는 코드 전반적인 수정 진행

--- src/main/java/com/sprint/project/monew/comment/controller/CommentController.java
+++ src/main/java/com/sprint/project/monew/comment/controller/CommentController.java
@@ -12,9 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
--- src/main/java/com/sprint/project/monew/comment/repository/CommentRepository.java
+++ src/main/java/com/sprint/project/monew/comment/repository/CommentRepository.java
@@ -4,7 +4,6 @@ import com.sprint.project.monew.comment.entity.Comment;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
