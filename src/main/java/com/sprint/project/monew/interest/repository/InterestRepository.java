34764dda1c509f0844c609421fd70b0ace1c03ed
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-30 11:22:16 +0900

    feat: 관심사 페이지네이션 변경

--- src/main/java/com/sprint/project/monew/interest/repository/InterestRepository.java
+++ src/main/java/com/sprint/project/monew/interest/repository/InterestRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InterestRepository extends JpaRepository<Interest, UUID> {
+public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestQueryRepository {
 
 
 }
