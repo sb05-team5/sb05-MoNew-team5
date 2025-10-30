52d0ea8009f5cbc2676db650bf758d2899f1b29e
loi2016 <loi2012@naver.com>
2025-10-30 14:31:23 +0900

    Feature/comment : 파생 컴파일/런타임 에러 수정

--- src/main/java/com/sprint/project/monew/comment/repository/CommentRepository.java
+++ src/main/java/com/sprint/project/monew/comment/repository/CommentRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,14 +14,6 @@ public interface CommentRepository
         extends JpaRepository<Comment, UUID>, CommentQueryRepository {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select c from Comment c where c.id = : id")
+    @Query("select c from Comment c where c.id = :id")
     Optional<Comment> findForUpdate(@Param("id") UUID id);
-
-    List<Comment> findOrderByDate(
-            UUID articleId, boolean sort, Instant dateCursor, UUID idCursor, int limit
-    );
-
-    List<Comment> findOrderByLikes(
-            UUID articleId, boolean sort, Integer likeCursor, UUID idCursor, int limit
-    );
 }
--- src/main/java/com/sprint/project/monew/commentLike/service/CommentLikeService.java
+++ src/main/java/com/sprint/project/monew/commentLike/service/CommentLikeService.java
@@ -18,48 +18,48 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CommentLikeService {
 
-//    private final CommentLikeRepository commentLikeRepository;
-//    private final CommentRepository commentRepository;
-//    private final UserRepository userRepository;
-//
-//    @Transactional
-//    public int commentLike(UUID commentId,UUID userId) {
-//
-//        if (commentLikeRepository.existsByComment_IdAndUser_Id(commentId, userId)) {
-//            return commentLikeRepository.countByComment_Id(commentId);
-//        }
-//
-//        Comment comment = commentRepository.findForUpdate(commentId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."));
-//
-//        if (comment.getDeletedAt() != null) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제된 댓글입니다.");
-//        }
-//
-//        User userRef = userRepository.getReferenceById(userId);
-//
-//        commentLikeRepository.save(CommentLike.create(comment, userRef));
-//
-//        comment.increaseLike();
-//
-//        return comment.getLikeCount();
-//    }
-//
-//    @Transactional
-//    public int uncommentLike(UUID commentId, UUID userId) {
-//        if (!commentLikeRepository.existsByComment_IdAndUser_Id(commentId, userId)) {
-//            return commentLikeRepository.countByComment_Id(commentId);
-//        }
-//
-//        Comment comment = commentRepository.findForUpdate(commentId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."));
-//        if (comment.getDeletedAt() != null) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제된 댓글입니다.");
-//        }
-//
-//        commentLikeRepository.deleteByComment_IdAndUser_Id(commentId, userId);
-//        comment.decreaseLike();
-//
-//        return comment.getLikeCount();
-//    }
+    private final CommentLikeRepository commentLikeRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public int commentLike(UUID commentId,UUID userId) {
+
+        if (commentLikeRepository.existsByComment_IdAndUser_Id(commentId, userId)) {
+            return commentLikeRepository.countByComment_Id(commentId);
+        }
+
+        Comment comment = commentRepository.findForUpdate(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."));
+
+        if (comment.getDeletedAt() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제된 댓글입니다.");
+        }
+
+        User userRef = userRepository.getReferenceById(userId);
+
+        commentLikeRepository.save(CommentLike.create(comment, userRef));
+
+        comment.increaseLike();
+
+        return comment.getLikeCount();
+    }
+
+    @Transactional
+    public int uncommentLike(UUID commentId, UUID userId) {
+        if (!commentLikeRepository.existsByComment_IdAndUser_Id(commentId, userId)) {
+            return commentLikeRepository.countByComment_Id(commentId);
+        }
+
+        Comment comment = commentRepository.findForUpdate(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."));
+        if (comment.getDeletedAt() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제된 댓글입니다.");
+        }
+
+        commentLikeRepository.deleteByComment_IdAndUser_Id(commentId, userId);
+        comment.decreaseLike();
+
+        return comment.getLikeCount();
+    }
 }
