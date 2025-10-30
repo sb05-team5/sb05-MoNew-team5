8c5e982ef9aa253514444148ff59d8c784616ef7
bustam00 <bustam080@gmail.com>
2025-10-30 13:46:50 +0900

    feat: mongo에 관심사 로그 생성 임시코드

--- src/main/java/com/sprint/project/monew/log/document/SubscriptionActivity.java
+++ src/main/java/com/sprint/project/monew/log/document/SubscriptionActivity.java
@@ -1,24 +1,37 @@
 package com.sprint.project.monew.log.document;
 
-import com.sprint.project.monew.interest.entity.Interest;
-import com.sprint.project.monew.user.entity.User;
-import java.time.Instant;
-import java.util.UUID;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
 
 @Document(collection = "subscriptions")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
+@Builder
 public class SubscriptionActivity {
 
-  private UUID id;
-  private Instant createdAt;
-  private Interest interest;
-  private User user; //보고 보류
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+    @Field("created_at")
+    private Instant createdAt;
+    @Field("interest_id")
+    private String interestId;
+
+    private String interestName;
+
+    private List<String> interestKeywords;
+
+    private Integer interestSubscripberCount;
+
+    @Field("user_id")
+    private String userId; //보고 보류
 
 }
