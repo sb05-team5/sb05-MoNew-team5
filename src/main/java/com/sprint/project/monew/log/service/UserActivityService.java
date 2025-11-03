10fb22a22fc4a389cb6aef3f8303971c6f712bd1
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-11-03 14:58:53 +0900

    feat: 댓글 좋아요 활동 내역 구현

--- src/main/java/com/sprint/project/monew/log/service/UserActivityService.java
+++ src/main/java/com/sprint/project/monew/log/service/UserActivityService.java
@@ -1,8 +1,11 @@
 package com.sprint.project.monew.log.service;
 
 import com.sprint.project.monew.log.document.CommentActivity;
+import com.sprint.project.monew.log.document.CommentLikeActivity;
 import com.sprint.project.monew.log.dto.CommentActivityDto;
+import com.sprint.project.monew.log.dto.CommentLikeActivityDto;
 import com.sprint.project.monew.log.repository.CommentActivityRepository;
+import com.sprint.project.monew.log.repository.CommentLikeActivityRepository;
 import com.sprint.project.monew.user.entity.User;
 import com.sprint.project.monew.user.repository.UserRepository;
 import java.util.List;
@@ -16,11 +19,11 @@ import org.springframework.stereotype.Service;
 public class UserActivityService {
 
   private final CommentActivityRepository commentActivityRepository;
+  private final CommentLikeActivityRepository commentLikeActivityRepository;
   private final UserRepository userRepository;
 
   public List<CommentActivityDto> getComments(UUID userId) {
-    User user = userRepository.findById(userId)
-        .orElseThrow(() -> new NoSuchElementException("Not found user"));
+    User user = validatedUserId(userId);
 
     return commentActivityRepository.findTop10ByUserIdOrderByCreatedAtDesc(user.getId().toString()).stream()
         .map(c -> CommentActivityDto.builder()
@@ -35,6 +38,32 @@ public class UserActivityService {
             .build())
         .toList();
   }
+
+  public List<CommentLikeActivityDto> getCommentLikes(UUID userId) {
+    User user = validatedUserId(userId);
+
+    return commentLikeActivityRepository.findTop10ByUserIdOrderByCreatedAtDesc(user.getId().toString()).stream()
+        .map(c -> CommentLikeActivityDto.builder()
+            .id(c.getId())
+            .createdAt(c.getCreatedAt())
+            .articleId(c.getArticleId())
+            .articleTitle(c.getArticleTitle())
+            .commentId(c.getCommentId())
+            .content(c.getContent())
+            .likeCount(c.getLikeCount())
+            .commentCreatedAt(c.getCommentCreatedAt())
+            .userId(c.getUserId())
+            .userName(c.getUserName())
+            .build())
+        .toList();
+  }
+
+  private User validatedUserId(UUID userId) {
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new NoSuchElementException("Not found user"));
+    return user;
+  }
+
 
 
 
